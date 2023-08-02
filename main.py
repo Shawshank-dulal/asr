7893c748b13ff64a06f4b005eb06c7f91ef66243
shawshank-dulal <sashank.dulal@gmail.com>
2023-08-02 17:52:46 +0545

    added video-audio conversion

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import whisper
+import subprocess
+
+print(subprocess.run('ffmpeg -i input.mp4 -b:a 256K -vn test.mp3'))
+
+
 
 model = whisper.load_model("small")
 result = model.transcribe("test.mp3")
